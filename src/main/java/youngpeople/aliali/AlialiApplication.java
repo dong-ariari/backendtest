cde9f4846f4a54ebcfbd2cb32fb3d1cb31df6440
Administrator <iuholic83@naver.com>
2024-05-02 01:39:36 +0900

    commit is success

---
+++ src/main/java/youngpeople/aliali/AlialiApplication.java
@@ -0,0 +1,13 @@
+package youngpeople.aliali;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlialiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlialiApplication.class, args);
+	}
+
+}
